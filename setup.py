fa02db982400e93667c1e331dfdb9a5da60a7a2c
yuripedro <yuripedro3@gmail.com>
2020-08-14 18:17:55 -0300

    inicial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+setup(
+    name='comm_sim',
+    version='0.1',
+    url='https://github.com/yuripedro/comm_sim', 
+    author='Yuri Pedro dos Santos',
+    author_email='yuripedro3@gmail.com',
+    description='comm_sim',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
